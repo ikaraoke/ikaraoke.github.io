4bd3a39cea16920777890b3cbd4a879c4e105af8
ikaraoke <68802938+ikaraoke@users.noreply.github.com>
2020-07-26 12:17:22 +0545

    Create initaudioplayer.js

---
+++ initaudioplayer.js
@@ -0,0 +1,127 @@
+jQuery(document).ready(function(){
+
+    var jsFolder = "https://amazingaudioplayer.com/wp-content/uploads/amazingaudioplayer/7/audioplayerengine/";
+
+    jQuery("#amazingaudioplayer-7").amazingaudioplayer({
+
+        jsfolder:jsFolder,
+
+        volumeimagewidth:24,
+
+        barbackgroundimage:"",
+
+        imagewidth:100,
+
+        showtime:true,
+
+        titleinbarwidth:80,
+
+        showprogress:true,
+
+        random:false,
+
+        titleformat:"%TITLE%",
+
+        height:164,
+
+        prevnextimage:"prevnext-48-48-0.png",
+
+        showinfo:true,
+
+        imageheight:100,
+
+        skin:"MusicBox",
+
+        loopimage:"loop-24-24-1.png",
+
+        loopimagewidth:24,
+
+        volumebarheight:80,
+
+        prevnextimageheight:48,
+
+        infoformat:"By %ARTIST% %ALBUM%<br />%INFO%",
+
+        showstop:false,
+
+        showvolumebar:true,
+
+        width:320,
+
+        showtitleinbar:false,
+
+        showloop:false,
+
+        volumeimage:"volume-24-24-1.png",
+
+        playpauseimagewidth:48,
+
+        loopimageheight:24,
+
+        tracklistitemformat:"%ORDER%. %TITLE% %DURATION%",
+
+        prevnextimagewidth:48,
+
+        tracklistarrowimage:"tracklistarrow-16-16-0.png",
+
+        playpauseimageheight:48,
+
+        showbackgroundimage:false,
+
+        progresswidthmode:"fixed",
+
+        stopimage:"stop-48-48-0.png",
+
+        showvolume:true,
+
+        playpauseimage:"playpause-48-48-0.png",
+
+        showprevnext:true,
+
+        backgroundimage:"",
+
+        volumebarpadding:8,
+
+        progressheight:8,
+
+        showtracklistbackgroundimage:false,
+
+        progresswidth:296,
+
+        showtitle:true,
+
+        tracklistarrowimageheight:16,
+
+        heightmode:"fixed",
+
+        titleinbarformat:"%TITLE%",
+
+        showtracklist:false,
+
+        stopimageheight:48,
+
+        volumeimageheight:24,
+
+        stopimagewidth:48,
+
+        tracklistbackgroundimage:"",
+
+        showbarbackgroundimage:false,
+
+        showimage:true,
+
+        tracklistwidth:320,
+
+        tracklistarrowimagewidth:16,
+
+        timeformat:"%CURRENT% / %DURATION%",
+
+        autoplay:false,
+
+        loop:1,
+
+        tracklistitem:10
+
+    });
+
+});
